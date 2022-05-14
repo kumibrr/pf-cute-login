528726354bf452197a74188094d3d1d713d136b7
kumibrr <isra.lora99@gmail.com>
2022-05-14 17:08:46 +0200

    Fixed styling

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -6,13 +6,16 @@
   <input type="text" name="user" id="user" placeholder="username"/>
 <div>
   <input type="password" name="password" id="password" placeholder="password"/>
-  <input type="submit" value="submit" />
+  <input class="submit" type="submit" value="submit" />
 </div>
 </form>
 
 <style>
   div {
     display: flex;
-    justify-content: space-between;
+  }
+
+  .submit {
+    margin-left: 5px;
   }
 </style>
