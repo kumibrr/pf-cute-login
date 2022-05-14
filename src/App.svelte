e7d7cf492fa88e78213bbbd93dfecf8749d0f347
kumibrr <isra.lora99@gmail.com>
2022-05-14 16:47:14 +0200

    Created base login component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
 <script>
-	export let name;
+	import Login from './components/login.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div>
+		<Login></Login>
+	</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
+	:root {
+		color-scheme: light dark;
+		accent-color: auto;
+	}
+
+	div {
+		background-color: rgb(144, 144, 144);
 		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		max-width: 20rem;
 	}
 </style>
